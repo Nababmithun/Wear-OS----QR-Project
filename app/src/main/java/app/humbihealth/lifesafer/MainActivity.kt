f3886ea37bdc188243249ad061d03388acc173fa
Nababmithun <monermithun789@gmail.com>
2023-05-30 13:36:53 -0400

    android configure

--- app/src/main/java/app/humbihealth/lifesafer/MainActivity.kt
+++ app/src/main/java/app/humbihealth/lifesafer/MainActivity.kt
@@ -1,7 +1,14 @@
 package app.humbihealth.lifesafer
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import app.humbihealth.lifesafer.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
@@ -13,6 +20,13 @@ class MainActivity : Activity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.ivQr.setOnClickListener {
+            val intent = Intent(this, ShareMyAccount::class.java)
+            startActivity(intent)
+        }
+        binding.ivSos.setOnClickListener {
+            Toast.makeText(this@MainActivity, "An SOS has sent", Toast.LENGTH_SHORT).show()
 
+        }
     }
 }
---
+++ app/src/main/java/app/humbihealth/lifesafer/ShareMyAccount.kt
@@ -0,0 +1,12 @@
+package app.humbihealth.lifesafer
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShareMyAccount : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share_my_account)
+    }
+}
